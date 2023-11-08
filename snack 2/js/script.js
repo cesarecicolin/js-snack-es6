79b6d4c07bce918ea2a64c1cbe6cca94be77fc4b
cesarecicolin <cece.caster@gmail.com>
2023-11-08 17:17:51 +0100

    snack 2 generazione punti

---
+++ snack 2/js/script.js
@@ -0,0 +1,37 @@
+const squadre = 
+[
+    {
+        name: "milan",
+        point: 0,
+        foul: 0
+    },
+    {
+        name: "juventus",
+        point: 0,
+        foul: 0
+    },
+    {
+        name: "inter",
+        point: 0,
+        foul: 0
+    },
+]
+console.log(squadre);
+
+const [milan, juventus, inter] = squadre
+
+
+
+squadre.forEach(squadre => {
+    squadre.point = randomPoint();
+    squadre.foul = randomPoint();
+  });
+
+  
+
+// \*******************
+// funzioni
+function randomPoint() {
+    const random = Math.floor(Math.random() * 100) + 1;
+    return random
+}
